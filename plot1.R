e5a76e88862a0a5bb439292d3845bd8c65aa2416
Barry Alexander <barry_alexander@gap.com>
2016-10-18 09:47:32 -0700

    [BA] wip

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-setwd("/Users/barryalexander/coursera/ExploratoryDataAnalysis/ExData_Plotting1")
+setwd("/Users/ba25714/ExData_Plotting1")
 
 if(!file.exists("household_power_consumption.txt")) {
   fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
@@ -6,13 +6,9 @@ if(!file.exists("household_power_consumption.txt")) {
   unzip("household_power_consumption.zip")
 }
 
+consumption <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+consumptionSubSet <- consumption[consumption$Date %in% c("1/2/2007","2/2/2007") ,]
 
-consumption <- read.table("household_power_consumption.txt",
-                          header = TRUE, sep = ";",
-                          na.strings = "?")
-
-consumption[complete.cases(consumption),]
-
-#consumption[,1] <- strptime(consumption[,1], "%d/%m/%Y")
+hist(as.numeric(consumptionSubSet$Global_active_power), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 
 
